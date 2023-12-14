b966bd0df873d4733ccb35e7ca7dcf75be61874d
Miguelrogenmoser4562 <miguel.rogenmoser@outlook.com>
2023-12-13 22:44:00 -0600

    othernew

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from .routes import app
 
 def create_app():
-    not_app = Flask(__name__)
+    not_app = Flask(__name__, template_folder='wordtwo/templates')
     not_app.register_blueprint(app)
     return not_app
